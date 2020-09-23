f0c30e0d9ced6e96f0d552b669057edcd614a7aa
Mehdi <jemai2000@hotmail.com>
2020-09-23 19:20:11 -0400

    video 10 Mettre en place les propriétés de navigation - Partie #1

---
+++ GestionStageEquipe7/Data/Migrations/20200923231208_AjoutClsTypeEmployeur.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionStageEquipe7.Data.Migrations
+{
+    public partial class AjoutClsTypeEmployeur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeEmployeurId",
+                table: "Employeur",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "TypesEmployeur",
+                schema: "dbo",
+                columns: table => new
+                {
+                    TypeEmployeurId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescriptionTypeEmployeur = table.Column<string>(maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypesEmployeur", x => x.TypeEmployeurId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employeur_TypeEmployeurId",
+                table: "Employeur",
+                column: "TypeEmployeurId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur",
+                column: "TypeEmployeurId",
+                principalSchema: "dbo",
+                principalTable: "TypesEmployeur",
+                principalColumn: "TypeEmployeurId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur");
+
+            migrationBuilder.DropTable(
+                name: "TypesEmployeur",
+                schema: "dbo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employeur_TypeEmployeurId",
+                table: "Employeur");
+
+            migrationBuilder.DropColumn(
+                name: "TypeEmployeurId",
+                table: "Employeur");
+        }
+    }
+}
---
+++ GestionStageEquipe7/Data/Migrations/20200923231736_AjoutValeurNulleTypeEmployeur.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionStageEquipe7.Data.Migrations
+{
+    public partial class AjoutValeurNulleTypeEmployeur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeEmployeurId",
+                table: "Employeur",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur",
+                column: "TypeEmployeurId",
+                principalSchema: "dbo",
+                principalTable: "TypesEmployeur",
+                principalColumn: "TypeEmployeurId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeEmployeurId",
+                table: "Employeur",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employeur_TypesEmployeur_TypeEmployeurId",
+                table: "Employeur",
+                column: "TypeEmployeurId",
+                principalSchema: "dbo",
+                principalTable: "TypesEmployeur",
+                principalColumn: "TypeEmployeurId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
