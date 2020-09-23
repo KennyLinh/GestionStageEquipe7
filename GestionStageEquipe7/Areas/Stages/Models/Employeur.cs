5953c8173c3dfff1faa3c8fc55cf3a53f9af8692
Mehdi <jemai2000@hotmail.com>
2020-09-23 15:22:59 -0400

    variable prop ajouter

---
+++ GestionStageEquipe7/Areas/Stages/Models/Employeur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionStageEquipe7.Areas.Stages.Models
+{
+    public class Employeur
+    {
+        public Guid intmployeurId { get; set; }
+        public string NomEmployeur { get; set; }
+        public bool Actif { get; set; }
+    }
+}
